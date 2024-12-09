6c6012d0d92ddc8874fa478a259b27a32f27d9bc
chebanroman-sigma <r3388967@gmail.com>
2024-12-09 16:19:05 +0200

    zadanie1
    zadanie1

---
+++ ConsoleApp4/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp4
+{
+    internal class Program
+    {
+        /// <summary>
+        /// Задача 1. Поменять местами содержимое двух строковых переменных.
+        /// </summary>
+        /// <param name="args"></param>
+        public static string Input(string message)
+        {
+            Console.Write(message);
+            return Console.ReadLine();
+        }
+
+        static void Main(string[] args)
+        {
+            string a = Input("a= ");
+            string b = Input("b= ");
+            Swap(ref a, ref b);
+            Console.WriteLine($"результат: {a}\t{b}");
+            Console.ReadKey();
+        }
+
+        public static void Swap(ref string a, ref string b)
+        {
+            string temp = a;
+            a = b;
+            b = temp;
+        }
+    }
+}
